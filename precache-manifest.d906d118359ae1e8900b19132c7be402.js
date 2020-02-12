28db279eef5148db798472a75c8f19061167f42c
Jarek-Trzybinski <jaroslaw.trzybinski@gmail.com>
2020-02-12 12:42:47 +0000

    Updates

--- precache-manifest.d906d118359ae1e8900b19132c7be402.js
+++ precache-manifest.d906d118359ae1e8900b19132c7be402.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = [
   {
-    "revision": "ead6c717ceab27748914",
+    "revision": "6e2fb87ade6be9ade281",
     "url": "/fend-p7-reactnd-project-myreads-app/static/css/main.dd36d5ed.chunk.css"
   },
   {
-    "revision": "ead6c717ceab27748914",
-    "url": "/fend-p7-reactnd-project-myreads-app/static/js/main.ead6c717.chunk.js"
+    "revision": "6e2fb87ade6be9ade281",
+    "url": "/fend-p7-reactnd-project-myreads-app/static/js/main.6e2fb87a.chunk.js"
   },
   {
     "revision": "1922525940d15b55a1b8",
@@ -20,15 +20,15 @@ self.__precacheManifest = [
     "url": "/fend-p7-reactnd-project-myreads-app/static/media/arrow-back.20e8847d.svg"
   },
   {
-    "revision": "75d390e5eabc3254418e950df631389b",
-    "url": "/fend-p7-reactnd-project-myreads-app/static/media/add.75d390e5.svg"
-  },
-  {
     "revision": "9d4e3f366f039249695de85a30a02b95",
     "url": "/fend-p7-reactnd-project-myreads-app/static/media/arrow-drop-down.9d4e3f36.svg"
   },
   {
-    "revision": "d181cdb23a3c1e2a4e0d72a10f90682f",
+    "revision": "75d390e5eabc3254418e950df631389b",
+    "url": "/fend-p7-reactnd-project-myreads-app/static/media/add.75d390e5.svg"
+  },
+  {
+    "revision": "af041249741a4be16e984068b8bb7542",
     "url": "/fend-p7-reactnd-project-myreads-app/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/fend-p7-reactnd-project-myreads-app/precache-manifest.ef67bc385cee52f16cd75d894c2c0d62.js"
+  "/fend-p7-reactnd-project-myreads-app/precache-manifest.d906d118359ae1e8900b19132c7be402.js"
 );
 
 workbox.clientsClaim();
